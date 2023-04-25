8981a44b846ff34644539207505c074963097e59
Paul Etienney <paul.etienney@beta.gouv.fr>
2023-04-25 09:59:59 +0200

    copy ok

--- components/VisuPanel.tsx
+++ components/VisuPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useContext } from 'react';
+import React, { useRef, useEffect, useContext, useState } from 'react';
 import {MapContext} from '@/components/MapContext'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { fr } from "@codegouvfr/react-dsfr";
@@ -8,9 +8,17 @@ import styles from './VisuPanel.module.css'
 export default function VisuPanel() {
 
     const [mapCtx, setMapCtx] = useContext(MapContext)
+    const [copied, setCopied] = useState(false);
 
     const hasBdg = () => {
         return mapCtx.data.panel_bdg?.rnb_id !== undefined
+    }
+
+    const handleCopy = () => {
+        setCopied(true)
+        setTimeout(() => {
+            setCopied(false)
+        }, 2000)
     }
 
     if (hasBdg()) {
@@ -20,13 +28,23 @@ export default function VisuPanel() {
                 <h6><b>Identifiant RNB</b></h6>
 
                 <div className={styles.rnbidShell}>
-                <div className={styles.rnbidShell__id}>{mapCtx.data.panel_bdg.rnb_id}</div>
-                <div>
-                <CopyToClipboard text={mapCtx.data.panel_bdg.rnb_id}>
-                    <span>Copier l'identifiant <span className={fr.cx("fr-icon-clipboard-line")}></span></span>
-                </CopyToClipboard>
+                    <div className={styles.rnbidShell__id}>{mapCtx.data.panel_bdg.rnb_id}</div>
+
+                    
+                    <CopyToClipboard 
+                        onCopy={() => handleCopy()}
+                        text={mapCtx.data.panel_bdg.rnb_id}>
+
+                    <div className={styles.rnbidShell__copy}>
+                        {copied ? <span>Copié <i className={fr.cx("fr-icon-success-line")}></i></span> : <span>Copier <i className={fr.cx("fr-icon-clipboard-line")}></i></span>}
+                        
+                        </div>
+                    
+                    </CopyToClipboard>
+
                 </div>
-                </div>
+
+                
                 
             </div>
             </>
