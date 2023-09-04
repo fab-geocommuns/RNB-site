ddeebb06f11ab00392e9e663c3652f02241d37e5
Paul Etienney <paul.etienney@beta.gouv.fr>
2023-09-04 17:15:20 +0200

    lien home vers chacun des cas

--- app/page.tsx
+++ app/page.tsx
@@ -130,9 +130,9 @@ export default function Home() {
                         <div className="fr-col-12 fr-col-md-4 ">
                         <Card
                         
-        desc="Plus de 2.7 millions de DPE ont été réalisés en France en 2022. Hors, le local dont le DPE fait l’objet est uniquement relié à une adresse et non à un bâtiment géolocalisé."
-        linkProps={{ href: "/doc" }}
-        title="Fiabiliser les Diagnostics de Performance Énergétique (DPE)"
+                            desc="Plus de 2.7 millions de DPE ont été réalisés en France en 2022. Hors, le local dont le DPE fait l’objet est uniquement relié à une adresse et non à un bâtiment géolocalisé."
+                            linkProps={{ href: "/cas/dpe" }}
+                            title="Fiabiliser les Diagnostics de Performance Énergétique (DPE)"
                         />
                         </div>
                         <div className="fr-col-12 fr-col-md-4 ">
@@ -140,7 +140,7 @@ export default function Home() {
                         <Card
                         
                         desc="Les gains en efficacité des procédures et les facilités qui en découlent laissent entrevoir un ROI global qui peut être estimé à plusieurs ETP au travers de toutes les directions” Toulouse Métropole - Direction du Numérique "
-                        linkProps={{ href: "/doc" }}
+                        linkProps={{ href: "/cas/communes" }}
                         title="Faciliter les échanges d’informations bâtimentaires au sein des communes"
                         />
                         </div>
@@ -149,7 +149,7 @@ export default function Home() {
                         <Card
                         
                         desc="La saisine d’un ID bâtiment lors du processus de référencement des ERP doit permettre de préciser leur géolocalisation, de faciliter et fiabiliser le processus de recensement et, d’aider le suivi des ERP."
-                        linkProps={{ href: "/ads" }}
+                        linkProps={{ href: "/cas/erp" }}
                         title="Améliorer l’identification des Etablissements Recevant du Public (ERP)"
                         />
     
