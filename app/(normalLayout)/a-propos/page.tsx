5b8d387f9520eb5f608ddccf1b1a1b85fb5a1dee
Paul Etienney <paul.etienney@beta.gouv.fr>
2024-01-18 15:57:43 +0100

    page à propos

--- app/(normalLayout)/a-propos/page.tsx
+++ app/(normalLayout)/a-propos/page.tsx
@@ -1,13 +1,17 @@
 // Comps
 import AboutCol from "@/components/AboutCol"
-import { Highlight } from "@codegouvfr/react-dsfr/Highlight";
 
+// Settings
+import settings from '@/logic/settings'
 
 
 
 
 
 export default function Page() {
+  
+    const contactEmail = settings.contactEmail;
+
     return (
       <div className="fr-container">
         
@@ -16,47 +20,21 @@ export default function Page() {
             
             <h1 className="text-blue">À propos du Référentiel National des Bâtiments</h1>
 
-            <h2>Le problème que nous tentons de résoudre</h2>
+            <h2>Problématique : le cloisennement de l&apos;information bâtimentaire</h2>
+            <p>Jusqu&apos;à présent, les données liées aux bâtiments sont détenues par une multitude d’acteurs, travaillant en silos. Les administrations, les collectivités et de nombreux acteurs privés (ex : fournisseurs d’énergie), œuvrent individuellement pour produire et obtenir l&pos;information sans que celle-ci soit mise en commun.</p>
+            <p>Cette absence de base de données nationale de référence sur le bâtiment engendre des ralentissements et des coûts importants dans la conduite d&apos;actions publiques et territoriales, pour répondre aux enjeux de la transition écologique ou du logement notamment.</p>
 
-            <Highlight>
-            “Du fait d&apos;un écosystème atomisé, la production de l&apos;information géolocalisée dans la sphère publique se traduit par un usage sous-optimal des deniers publics.”<br />- Rapport d&apos;Information, Sénat, Commission des Finances pour l&apos;IGN, 5 Octobre 2022
-            </Highlight>
+            <h2>Solution : le RNB, la plaque d&apos;immatriculation du bâtiment</h2>
+            <p>Le Référentiel national des bâtiments (RNB) est un service public numérique, qui a pour vocation de créer une nouvelle donnée nationale de référence du bâtiment, inexistante jusqu&apos;ici et pourtant essentielle à la conduite de nombreuses politiques publiques. </p>
+            <p>Pour cela, il répertorie l&apos;ensemble des bâtiments du territoire et leur associe un identifiant unique. Cette donnée pivot, composée d’une suite de 12 caractères alphanumériques, permet d’être facilement reproductible et transmissible par une personne ou un logiciel. </p>
+            <p>Une fois apposée au bâtiment, cette véritable plaque d&apos;immatriculation du bâtiment permet de simplifier le suivi et le croisement de différents jeux de données bâtimentaires pour les administrations publiques, les collectivités et les acteurs privés.</p>
 
-            <p>Parmi ces données géolocalisées, l&apos;information relative au bâtiment se démarque par l&apos;absence de référentiel. Le concept de bâtiment ne faisant pas consensus, les acteurs travaillent en silos répondant chacun à leur propre vision du bâtiment…</p>
-            <p>Cette absence se traduit par des informations rattachées à l&apos;adresse, au logement ou parfois à la parcelle cadastrale. L&apos;objet bâtiment apparaît alors comme le maillon manquant au socle de données géolocalisées structuré qui relie les concepts d&apos;adresses/bâtiment/locaux (parcelles).</p>
-            <p>Ainsi, les données géolocalisées actuelles ne permettent pas un pilotage satisfaisant des politiques publiques, en particulier celles qui concernent la rénovation énergétique, qui nécessitent une vision exhaustive, représentative, actualisée et partagée du parc.</p>
+            <h2>Méthode : Co-construire ensemble ce nouveau géocommun</h2>
 
-
-            <h2>Notre solution : un Référentiel National des Bâtiments</h2>
-
-            <p>La création d&apos;un référentiel unique des bâtiments, en lien avec les autres référentiels (adresses, locaux, parcelles) existants est nécessaire pour passer d&apos;un “écosystème atomisé” à un fonctionnement en réseau.</p>
-
-<p>C&apos;est dans ce contexte qu&apos;a vu le jour le projet de construction du Référentiel National des Bâtiments (RNB) appelé “Bat-ID”, d&apos;abord au sein du programme EIG en 2021, sous l&apos;impulsion de l&apos;ADEME et du CSTB. Ces sponsors historiques ont ensuit été rejoints par l&apos;IGN et la DGALN en 2023, lors de l&apos;évolution du projet en Start-Up d&apos;État désormais incubée à la Fabrique des Géocommuns de l&apos;IGN.</p>
-
-<p>L&apos;objectif est de co-construire le RNB avec l&apos;ensemble des parties prenantes, à destination des acteurs publics comme privés. Ce référentiel sera alimenté par les processus administratifs existants afin d&apos;aboutir à une mise à jour en continue des bases de données socles.</p>
-
-            
-
-            
-
-            <h2>Comment co-construire un Référentiel National des Bâtiments ?</h2>
-
-            <p>La stratégie du RNB repose sur deux approches complémentaires :</p>
-            <ul>
-            <li>D&apos;une part, expérimenter au maximum afin d&apos;embarquer les acteurs, démontrer les usages, en créant les boucles de rétroaction de l&apos;alimentation du référentiel.</li>
-            <li>De l&apos;autre, co-construire une gouvernance commune aux parties prenantes, afin de susciter l&apos;adhésion et créer le cadre favorable à la vie du référentiel.</li>
-            </ul>
-
-            <p>L&apos;approche expérimentale permet de cerner précisément les besoins et points de friction à l&apos;émergence du projet, grâce à une vision terrain et à la mise en œuvre technique. La démarche de co-construction, permet de fédérer les acteurs pour rendre possible l&apos;interopérabilité à l&apos;échelle nationale en permettant le partage et l&apos;échange de données.</p>
-            <p>L&apos;objectif à terme : aboutir à un modèle de gouvernance pérenne, pour faire de cette future donnée de référence un véritable géo-commun.</p>
-            <p>Vous avez une idée ? Une question ? Vous souhaitez contribuer au projet ou devenir un de nos partenaires ?</p>
-            <p>N&apos;hésitez pas à nous écrire à <a href="mailto:rnb@beta.gouv.fr">rnb@beta.gouv.fr</a></p>
-
-            <hr />
-
-            <h2>La démarche start-up d&apos;État</h2>
-            <p>Depuis juin 2013, l&apos;État a expérimenté une nouvelle manière de construire des services publics numériques. Cette expérience a commencé au sein du Secrétariat Général pour la Modernisation de l&apos;Action Publique (SGMAP) par la refonte du site data.gouv.fr et s&apos;est généralisée avec la création en septembre 2015 d&apos;une structure dédiée : l&apos;Incubateur de Services Numériques beta.gouv.fr. Cette mission a été confiée à la Direction interministérielle du numérique et du système d&apos;information et de communication de l&apos;État (DINSIC).</p>
-            <p>Les changements de société et les évolutions technologiques imposent de repenser l&apos;interaction entre l&apos;administration et les citoyens. De nouvelles opportunités s&apos;ouvrent pour rendre un service gratuit, direct et innovant aux usagers. C&apos;est la volonté des start-up d&apos;État qui, sans capital privé et sans volonté lucrative, agissent dans la simplicité et avec un esprit neuf pour développer des solutions à vos problèmes quotidiens. Pour s&apos;assurer de ne pas tomber dans les travers de la bureaucratie, les projets se développent loin de toute contrainte. Une Startup d&apos;État c&apos;est donc une équipe allant de 2 à 8 personnes totalement autonomes qui travaillent pour votre service : tout simplement !</p>
+            <p>Le financement du RNB est assuré conjointement par l&apos;Agence de la Transition Écologique (<a href="https://www.ademe.fr/" target="_blank">ADEME</a>), le Centre Scientifique et Technique du Bâtiment (<a href="https://www.cstb.fr/fr/" target="_blank">CSTB</a>), la Direction générale de l&apos;Aménagement, du Logement et de la Nature (<a href="https://www.ecologie.gouv.fr/direction-generale-lamenagement-du-logement-et-nature-dgaln" target="_blank">DGALN</a>) et l&apos;Institut national de l&apos;information géographique et forestière (<a href="https://www.ign.fr/" target="_blank">IGN</a>).</p>
+            <p>Le service est réalisé à la fabrique des géocommuns, l&apos;incubateur de l&apos;IGN, appliquant la méthode du programme <a href="https://beta.gouv.fr/" target="_blank">beta.gouv</a> de la Direction Interministérielle du Numérique (<a href="https://www.numerique.gouv.fr/dinum/" target="_blank">DINUM</a>).</p>
+            <p>Les standards du RNB sont également réalisés en collaboration avec les experts de la donnée géomatique du Conseil National de l&apos;Information Géolocalisée (<a href="https://cnig.gouv.fr/gt-bati-a25939.html" target="_blank">CNIG</a>).</p>
+            <p>Vous souhaitez contribuer ? Toutes les contributions sont les bienvenues pour construire ce géocommun, faciliter et réduire les coûts de maintenance et optimiser la qualité des données. Contactez nous sur <a href={contactEmail}>{contactEmail}</a></p>
 
           </div>
           <div className="fr-col-12 fr-col-md-3 fr-col-offset-md-1">
