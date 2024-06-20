0a7f5a6a8527e369c2cde66aebe6029a9505df86
Paul Etienney <paul.etienney@beta.gouv.fr>
2024-06-20 15:35:33 +0200

    remove white lines

--- components/DBCard.tsx
+++ components/DBCard.tsx
@@ -29,50 +29,35 @@ export default function Entry({db, }) {
 
     return (
         <>
-            
-            
+            <div className={styles.card} id={db.key}>
 
-                <div className={styles.card} id={db.key}>
-
-                    <div className={styles.logoShell}>
+                <div className={styles.logoShell}>
                     <ImageNext className={styles.logo} width="30" height="30" src={imagePath(db.image)} alt={db.name} />
-                    </div>
-                    <div className={styles.body}>
-
-
-                        <div className={styles.titleBlock}>
-                            <h3 className={styles.title}><a onClick={trackDbClick(db)} href={db.url}>{db.name}</a></h3>
-                            <div className={styles.meta}>
-                                <span>Editée par {db.published_by}</span>
-                                <span>{db.licence}</span>
-                            </div>
-                        </div>
-                        
-                        
-                        <p className={styles.description}>
-                            {db.description}
-                        </p>
-                        <div>
-                            {db.tags.map((tag) => (
-                                <span className={styles.tagShell} key={tag}><Badge severity='info' noIcon small>{tag}</Badge></span>
-                                
-                            ))}
-                        </div>
-                        
-                    </div>
-
-                    
-                    
-                
-
                 </div>
                 
+                <div className={styles.body}>
 
-            
-
-
-            
-            
+                    <div className={styles.titleBlock}>
+                        <h3 className={styles.title}><a onClick={trackDbClick(db)} href={db.url}>{db.name}</a></h3>
+                        <div className={styles.meta}>
+                            <span>Editée par {db.published_by}</span>
+                            <span>{db.licence}</span>
+                        </div>
+                    </div>
+                    
+                    <p className={styles.description}>
+                        {db.description}
+                    </p>
+                    
+                    <div>
+                        {db.tags.map((tag) => (
+                            <span className={styles.tagShell} key={tag}><Badge severity='info' noIcon small>{tag}</Badge></span>
+                            
+                        ))}
+                    </div>
+                    
+                </div>
+            </div>
         </>
     )
 
