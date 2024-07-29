33cef05fa89c8a11eb0e82b806d17e6327529073
Bastien <bastien.baret@gmail.com>
2024-07-29 16:59:07 +0200

    Dans la FAQ, forcer le défilement de la page sur la bonne section en fonction du hash présent dans l'URL.

--- components/faq/SectionsList.tsx
+++ components/faq/SectionsList.tsx
@@ -1,23 +1,31 @@
+'use client';
 import styles from '@/styles/faq.module.scss';
 
 import Entry from '@/components/faq/Entry';
+import { useEffect } from 'react';
 
 export default function FaqSections(sections: any) {
+  useEffect(() => {
+    if (window.location.hash) {
+      const id = window.location.hash.split('#')[1];
+      const element = document.getElementById(id);
+      if (element) {
+        setTimeout(() => element.scrollIntoView());
+      }
+    }
+  }, []);
+
   return (
     <>
       {sections.sections.map((s: any) => {
         return (
-          <>
-            <div className={styles.faq__section}>
-              <h2 className={styles.faq__sectionTitle} key={s.key} id={s.key}>
-                {s.name}
-              </h2>
+          <div className={styles.faq__section} key={s.key} id={s.key}>
+            <h2 className={styles.faq__sectionTitle}>{s.name}</h2>
 
-              {s.entries.map((e: any) => {
-                return <Entry key={e.key} entry={e} />;
-              })}
-            </div>
-          </>
+            {s.entries.map((e: any) => {
+              return <Entry key={e.key} entry={e} />;
+            })}
+          </div>
         );
       })}
     </>
