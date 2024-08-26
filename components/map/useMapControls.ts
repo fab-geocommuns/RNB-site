a0b0d6a654926e4b2e75f52aaa1e182b2811b7cf
Paul Etienney <paul.etienney@beta.gouv.fr>
2024-08-26 11:35:42 +0200

    retrait de l'orientation forcée - test sur mobile

--- components/map/useMapControls.ts
+++ components/map/useMapControls.ts
@@ -4,11 +4,6 @@ import { fr } from '@codegouvfr/react-dsfr';
 import MapStyleSwitcherControl from '@/components/map/MapStyleSwitcher';
 import { STYLES } from '@/components/map/useMapLayers';
 
-interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
-  requestPermission?: () => Promise<'granted' | 'denied'>;
-  webkitCompassHeading?: number;
-}
-
 /**
  * Ajout et gestion des controles de la carte
  * @param map
@@ -38,30 +33,12 @@ export const useMapControls = (map?: maplibregl.Map) => {
       // Zoom
       map.addControl(
         new maplibregl.NavigationControl({
-          showCompass: false,
+          showCompass: true,
           showZoom: true,
         }),
         'bottom-right',
       );
 
-      // Geo localisation
-      const handleOrientation = (event: DeviceOrientationEventiOS) => {
-        let alpha = event.alpha || 0; // Angle en degrés autour de l'axe Z (boussole)
-        if (alpha !== null) {
-          // Pour obtenir une orientation précise par rapport au Nord, nous devons
-          // prendre en compte la boussole et les corrections nécessaires.
-          if (event.webkitCompassHeading) {
-            // Pour les appareils iOS
-            alpha = event.webkitCompassHeading;
-          } else {
-            // Pour les autres appareils, inverser alpha
-            alpha = 360 - alpha;
-          }
-          map.setBearing(alpha, {
-            geolocateSource: true, // Empêche certains événements internes au composant qui désactivent le tracking de l'utilisateur
-          });
-        }
-      };
       const geolocControl = new maplibregl.GeolocateControl({
         positionOptions: {
           enableHighAccuracy: true,
@@ -75,33 +52,7 @@ export const useMapControls = (map?: maplibregl.Map) => {
           duration: 0,
         },
       });
-      geolocControl.on('trackuserlocationstart', () => {
-        // iOS 13+: demande de droits
-        const DeviceEvent =
-          DeviceMotionEvent as unknown as DeviceOrientationEventiOS;
-        if (typeof DeviceEvent.requestPermission === 'function') {
-          DeviceEvent.requestPermission()
-            .then((state) => {
-              if (state === 'granted') {
-                window.addEventListener(
-                  'deviceorientation',
-                  handleOrientation,
-                  true,
-                );
-              }
-            })
-            .catch(console.error);
-        } else {
-          window.addEventListener('deviceorientation', handleOrientation, true);
-        }
-      });
-      geolocControl.on('trackuserlocationend', () => {
-        window.removeEventListener(
-          'deviceorientation',
-          handleOrientation,
-          true,
-        );
-      });
+
       map.addControl(geolocControl, 'bottom-right');
     }
   }, [map]);
