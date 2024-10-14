24d1ea8803b85e91181a9a4b14102830f5dd17de
Bastien <bastien.baret@gmail.com>
2024-10-14 11:21:50 +0200

    Ajout du bouton de sélection du style d'affichage des bâtiments (point ou emprise au sol)

--- tests/fixtures/pages/map-page.ts
+++ tests/fixtures/pages/map-page.ts
@@ -4,11 +4,9 @@ import { test } from '@/tests/fixtures';
 
 export class MapPage extends RNBPage {
   readonly map: Locator;
-  readonly buildingStyleButton: Locator;
 
   constructor(page: Page) {
     super(page, '/carte');
     this.map = page.getByLabel('Map');
-    this.buildingStyleButton = page.getByTestId('control-point-emprise');
   }
 }
--- tests/map-page.spec.ts
+++ tests/map-page.spec.ts
@@ -12,26 +12,5 @@ test.describe('Carte', () => {
     );
 
     await expect(mapPage.map).toBeVisible();
-    await expect(mapPage.buildingStyleButton).toBeVisible();
-  });
-
-  test("doit changer de mode d'affichage entre point et emprise", async ({
-    mapPage,
-    browserName,
-  }) => {
-    test.skip(
-      browserName === 'firefox',
-      'Pas de support de WebGL2 sur Firefox headless',
-    );
-
-    let imageUrl = await mapPage.buildingStyleButton
-      .locator('img')
-      .getAttribute('src');
-    expect(imageUrl).toContain('/dot.');
-    await mapPage.buildingStyleButton.click();
-    imageUrl = await mapPage.buildingStyleButton
-      .locator('img')
-      .getAttribute('src');
-    expect(imageUrl).toContain('/polygon.');
   });
 });
