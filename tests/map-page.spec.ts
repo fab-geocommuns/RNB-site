27cf01f8af559903a2e68e7d979673343a02a64b
Bastien <bastien.baret@gmail.com>
2024-10-15 15:36:17 +0200

    Ajout du bouton de sélection du style d'affichage des bâtiments (point ou emprise au sol)

--- tests/map-page.spec.ts
+++ tests/map-page.spec.ts
@@ -12,5 +12,27 @@ test.describe('Carte', () => {
     );
 
     await expect(mapPage.map).toBeVisible();
+    await expect(mapPage.buildingStyleButton).toBeVisible();
+  });
+
+  test("doit changer de mode d'affichage entre point et emprise", async ({
+    mapPage,
+    browserName,
+  }) => {
+    test.skip(
+      browserName === 'firefox',
+      'Pas de support de WebGL2 sur Firefox headless',
+    );
+
+    let imageUrl = await mapPage.buildingStyleButton
+      .locator('img')
+      .getAttribute('src');
+    expect(imageUrl).toContain('/dot.');
+    await mapPage.buildingStyleButton.click();
+
+    imageUrl = await mapPage.buildingStyleButton
+      .locator('img')
+      .getAttribute('src');
+    expect(imageUrl).toContain('/polygon.');
   });
 });
