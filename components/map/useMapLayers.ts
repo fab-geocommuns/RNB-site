d6c7084d4d33dfc533be97986be2b3b68f973896
Paul Etienney <paul.etienney@beta.gouv.fr>
2024-10-15 16:01:50 +0200

    reduce ads icon size

--- components/map/useMapLayers.ts
+++ components/map/useMapLayers.ts
@@ -75,8 +75,8 @@ export const useMapLayers = (map?: maplibregl.Map) => {
         'circle-radius': [
           'case',
           ['boolean', ['==', ['feature-state', 'hovered'], true]],
-          14,
           12,
+          10,
         ],
         'circle-stroke-color': [
           'case',
@@ -106,7 +106,7 @@ export const useMapLayers = (map?: maplibregl.Map) => {
           'adsModify',
           'adsBuild',
         ],
-        'icon-size': 0.25,
+        'icon-size': 0.2,
         'icon-allow-overlap': true,
         'icon-ignore-placement': true,
       },
