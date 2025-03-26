1a329220f0882a099f3c3f4347d4168394852d22
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-03-26 16:46:04 +0100

    Simple page votre-compte
    Simple page pour démo les informations utilisateurs, par exemple pour
    rediriger une fois la création d'utilisateur faite

---
+++ app/(normalLayout)/votre-compte/page.tsx
@@ -0,0 +1,10 @@
+import AccountInformation from '@/components/AccountInformation';
+
+export default async function VotreComptePage() {
+  return (
+    <div>
+      <h1>Votre compte</h1>
+      <AccountInformation />
+    </div>
+  );
+}
