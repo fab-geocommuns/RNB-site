6a85dfdc9ef7f4d0d5a855e5d937f306a24384d7
Francis Chabouis <fchabouis@gmail.com>
2025-04-02 17:50:33 +0200

    show status dropdown

--- components/contribution/EditionPanel.tsx
+++ components/contribution/EditionPanel.tsx
@@ -2,16 +2,70 @@ import styles from '@/styles/editPanel.module.scss';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/stores/store';
 import { SelectedBuilding } from '@/stores/map/map-slice';
+import { Select } from '@codegouvfr/react-dsfr/SelectNext';
+import { useState } from 'react';
 
 function EditSelectedBuildingPanel({
   selectedBuilding,
 }: {
   selectedBuilding: SelectedBuilding;
 }) {
+  const [newStatus, setNewStatus] = useState<string>(selectedBuilding.status);
+  const anyChanges = newStatus !== selectedBuilding.status;
+
   return (
     <div className={styles.content}>
-      Sélectionné : {selectedBuilding.rnb_id}
+      <RNBIDheader rnb_id={selectedBuilding.rnb_id}></RNBIDheader>
+      <Status status={newStatus} onChange={setNewStatus}></Status>
+      <button disabled={!anyChanges}>Valider les modifications</button>
     </div>
+  );
+}
+
+function RNBIDheader({ rnb_id }: { rnb_id: string }) {
+  return (
+    <>
+      Identifiant RNB
+      {rnb_id}
+    </>
+  );
+}
+
+function Status({
+  status,
+  onChange,
+}: {
+  status: string;
+  onChange: (status: string) => void;
+}) {
+  const statusList = [
+    {
+      label: 'Construit',
+      value: 'constructed',
+    },
+    {
+      label: 'En ruine',
+      value: 'notUsable',
+    },
+    {
+      label: 'Démoli',
+      value: 'demolished',
+    },
+  ];
+
+  return (
+    <>
+      <Select
+        nativeSelectProps={{
+          value: status,
+          onChange: (event) => {
+            onChange(event.target.value);
+          },
+        }}
+        label="Statut du bâtiment"
+        options={statusList}
+      />
+    </>
   );
 }
 
