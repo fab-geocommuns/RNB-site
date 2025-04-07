d6a62839fd95411cca1c2e0218aba074cb7689eb
Francis Chabouis <fchabouis@gmail.com>
2025-04-07 15:17:28 +0200

    first try to save a building modification

--- components/contribution/EditionPanel.tsx
+++ components/contribution/EditionPanel.tsx
@@ -5,6 +5,9 @@ import { SelectedBuilding } from '@/stores/map/map-slice';
 import { useState } from 'react';
 import RNBIDHeader from './RNBIDHeader';
 import BuildingStatus from './BuildingStatus';
+import { useRNBFetch } from '@/utils/use-rnb-fetch';
+import { Notice } from '@codegouvfr/react-dsfr/Notice';
+import { useEffect } from 'react';
 
 function PanelBody({ children }: { children: React.ReactNode }) {
   return <div className={styles.body}>{children}</div>;
@@ -16,7 +19,50 @@ function EditSelectedBuildingPanelContent({
   selectedBuilding: SelectedBuilding;
 }) {
   const [newStatus, setNewStatus] = useState<string>(selectedBuilding.status);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
   const anyChanges = newStatus !== selectedBuilding.status;
+  const { fetch } = useRNBFetch();
+
+  useEffect(() => {
+    if (error || success) {
+      const timer = setTimeout(() => {
+        setError(false);
+        setSuccess(false);
+      }, 4000); // 4 secondes
+
+      return () => clearTimeout(timer);
+    }
+  }, [error, success]);
+
+  const handleSubmit = async () => {
+    // setLoading(true);
+    // setError(null);
+    setSuccess(false);
+    const url = `${process.env.NEXT_PUBLIC_API_BASE}/buildings/${selectedBuilding.rnb_id}/`;
+
+    try {
+      const response = await fetch(url, {
+        body: JSON.stringify({
+          status: newStatus,
+        }),
+        method: 'PATCH',
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erreur ${response.status}`);
+      }
+
+      setSuccess(true);
+    } catch (err: any) {
+      setError(
+        err.message || 'La modification a échoué pour une raison inconnue',
+      );
+    } finally {
+      // setLoading(false);
+      console.log('fin');
+    }
+  };
 
   return (
     <>
@@ -26,8 +72,18 @@ function EditSelectedBuildingPanelContent({
           status={newStatus}
           onChange={setNewStatus}
         ></BuildingStatus>
-        <button disabled={!anyChanges}>Valider les modifications</button>
+        <button onClick={handleSubmit} disabled={!anyChanges}>
+          Valider les modifications
+        </button>
       </PanelBody>
+
+      <div className={styles.noticeContainer}>
+        <div
+          className={`${styles.notice} ${success || error ? styles.noticeVisible : ''}`}
+        >
+          <Notice title="Modifications enregistrées" severity="warning" />
+        </div>
+      </div>
     </>
   );
 }
