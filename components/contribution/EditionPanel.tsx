5f6978ab118f997fd51d7e8d349c8be4a365af95
Francis Chabouis <fchabouis@gmail.com>
2025-04-07 16:32:23 +0200

    revert commits pushed by error

--- components/contribution/EditionPanel.tsx
+++ components/contribution/EditionPanel.tsx
@@ -5,9 +5,6 @@ import { SelectedBuilding } from '@/stores/map/map-slice';
 import { useState } from 'react';
 import RNBIDHeader from './RNBIDHeader';
 import BuildingStatus from './BuildingStatus';
-import { useRNBFetch } from '@/utils/use-rnb-fetch';
-import { Notice } from '@codegouvfr/react-dsfr/Notice';
-import { useEffect } from 'react';
 
 function PanelBody({ children }: { children: React.ReactNode }) {
   return <div className={styles.body}>{children}</div>;
@@ -19,46 +16,7 @@ function EditSelectedBuildingPanelContent({
   selectedBuilding: SelectedBuilding;
 }) {
   const [newStatus, setNewStatus] = useState<string>(selectedBuilding.status);
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(null);
   const anyChanges = newStatus !== selectedBuilding.status;
-  const { fetch } = useRNBFetch();
-
-  useEffect(() => {
-    if (error || success) {
-      // show the message for a few seconds
-      const timer = setTimeout(() => {
-        setError(null);
-        setSuccess(false);
-      }, 4000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [error, success]);
-
-  const handleSubmit = async () => {
-    setError(null);
-    setSuccess(false);
-    const url = `${process.env.NEXT_PUBLIC_API_BASE}/buildings/${selectedBuilding.rnb_id}/`;
-
-    try {
-      const response = await fetch(url, {
-        body: JSON.stringify({
-          status: newStatus,
-        }),
-        method: 'PATCH',
-      });
-
-      if (!response.ok) {
-        throw new Error(`Erreur ${response.status}`);
-      }
-
-      setSuccess(true);
-    } catch (err: any) {
-      setError(err.message || 'Erreur lors de la modification');
-      console.log(error);
-    }
-  };
 
   return (
     <>
@@ -68,23 +26,8 @@ function EditSelectedBuildingPanelContent({
           status={newStatus}
           onChange={setNewStatus}
         ></BuildingStatus>
-        <button onClick={handleSubmit} disabled={!anyChanges}>
-          Valider les modifications
-        </button>
+        <button disabled={!anyChanges}>Valider les modifications</button>
       </PanelBody>
-
-      <div className={styles.noticeContainer}>
-        <div
-          className={`${styles.notice} ${success || error ? styles.noticeVisible : ''}`}
-        >
-          {success && (
-            <Notice title="Modification enregistrée" severity="info" />
-          )}
-          {error && (
-            <Notice title="Modification impossible" severity="warning" />
-          )}
-        </div>
-      </div>
     </>
   );
 }
