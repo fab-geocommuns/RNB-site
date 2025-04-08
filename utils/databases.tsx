8ac28139d419b6b57e081482d5ffef53747705a9
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-04-08 15:50:05 +0200

    Trying to bypass next build issues in CI

--- utils/databases.tsx
+++ utils/databases.tsx
@@ -1,11 +1,17 @@
 import { DiffusionDatabase } from '@/components/diffusionDatabase.type';
 
 export async function getDatabases(): Promise<DiffusionDatabase[]> {
-  const url = process.env.NEXT_PUBLIC_API_BASE + '/diffusion_databases';
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.sort(
-    (a: DiffusionDatabase, b: DiffusionDatabase) =>
-      a.display_order - b.display_order,
-  );
+  let result = [];
+
+  if (!process.env.CI) {
+    const url = process.env.NEXT_PUBLIC_API_BASE + '/diffusion_databases';
+    const res = await fetch(url);
+    const data = await res.json();
+    result = data.sort(
+      (a: DiffusionDatabase, b: DiffusionDatabase) =>
+        a.display_order - b.display_order,
+    );
+  }
+
+  return result;
 }
