a210cd5fdb2ed8dd9218b366ebd6bad03d9238a7
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-04-14 14:57:54 +0200

    Working list

--- components/contribution/BuildingAddress.tsx
+++ components/contribution/BuildingAddress.tsx
@@ -1,7 +1,41 @@
+import { BuildingAddress as BuildingAddressType } from '@/stores/map/map-slice';
+import Button from '@codegouvfr/react-dsfr/Button';
+
 type BuildingAddressProps = {
-  text: string;
+  address: BuildingAddressType;
+  onChange?: (address: BuildingAddressType) => void;
+  onRemove?: (address: BuildingAddressType) => void;
+  readOnly?: boolean;
 };
 
-export default function BuildingAddress({ text }: BuildingAddressProps) {
-  return <div>{text}</div>;
+export default function BuildingAddress({
+  address,
+  onChange,
+  onRemove,
+  readOnly = false,
+}: BuildingAddressProps) {
+  const formattedAddress = `${address.street_number}${address.street_rep} ${address.street}, ${address.city_zipcode} ${address.city_name}`;
+  return (
+    <div>
+      {formattedAddress}
+      {onChange && (
+        <Button
+          size="small"
+          onClick={() => onChange(address)}
+          iconId="fr-icon-edit-line"
+        >
+          <span></span>
+        </Button>
+      )}
+      {onRemove && (
+        <Button
+          size="small"
+          onClick={() => onRemove(address)}
+          iconId="fr-icon-delete-line"
+        >
+          <span></span>
+        </Button>
+      )}
+    </div>
+  );
 }
