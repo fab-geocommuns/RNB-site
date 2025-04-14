5635e8b4e058b3fc760ab39fa7957abddc10e7a3
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-04-14 11:41:51 +0200

    Revert trying to bypass requests in CI to build

--- utils/databases.tsx
+++ utils/databases.tsx
@@ -1,17 +1,11 @@
 import { DiffusionDatabase } from '@/components/diffusionDatabase.type';
 
 export async function getDatabases(): Promise<DiffusionDatabase[]> {
-  let result = [];
-
-  if (!process.env.CI) {
-    const url = process.env.NEXT_PUBLIC_API_BASE + '/diffusion_databases';
-    const res = await fetch(url);
-    const data = await res.json();
-    result = data.sort(
-      (a: DiffusionDatabase, b: DiffusionDatabase) =>
-        a.display_order - b.display_order,
-    );
-  }
-
-  return result;
+  const url = process.env.NEXT_PUBLIC_API_BASE + '/diffusion_databases';
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.sort(
+    (a: DiffusionDatabase, b: DiffusionDatabase) =>
+      a.display_order - b.display_order,
+  );
 }
