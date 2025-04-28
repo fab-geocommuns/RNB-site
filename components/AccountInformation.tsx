cb73b6bce7d38456bb0dbf0f6b037bc31f18bd59
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-04-28 17:31:05 +0200

    Afficher les tokens dans votre compte

--- components/AccountInformation.tsx
+++ components/AccountInformation.tsx
@@ -1,16 +1,32 @@
 'use client';
 
 import { useRNBAuthentication } from '@/utils/use-rnb-authentication';
+import { useRNBFetch } from '@/utils/use-rnb-fetch';
+import useRNBQuery from '@/utils/useRNBQuery';
+import { useEffect, useState } from 'react';
+
+type TokensResponse = {
+  productionToken: string;
+  sandboxToken: string;
+};
 
 export default function AccountInformation() {
   const { user } = useRNBAuthentication({ require: true });
+  const { isLoading, error, data } = useRNBQuery<TokensResponse>({
+    url: () => (user ? '/auth/users/me/tokens' : null),
+    method: 'GET',
+  });
 
   if (!user) return <p>Chargement en cours</p>;
+  if (isLoading) return <p>Chargement en cours</p>;
 
   return (
     <div>
       <p>
         Connecté en tant que <pre>{user.username}</pre>
+      </p>
+      <p>
+        <pre>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
       </p>
     </div>
   );
