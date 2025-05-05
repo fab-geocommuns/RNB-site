257dfb82f6d1697cc551bd947d90cb9c4b6eb857
Francis Chabouis <fchabouis@gmail.com>
2025-05-05 17:48:25 +0200

    create a BuildingShape component

--- components/contribution/EditionPanel.tsx
+++ components/contribution/EditionPanel.tsx
@@ -6,14 +6,12 @@ import { useState, useEffect } from 'react';
 import RNBIDHeader from './RNBIDHeader';
 import BuildingStatus from './BuildingStatus';
 import BuildingAddresses from './BuildingAddresses';
+import BuildingShape from './BuildingShape';
 import { useRNBFetch } from '@/utils/use-rnb-fetch';
 import { Notice } from '@codegouvfr/react-dsfr/Notice';
 import { geojsonToWKT } from '@terraformer/wkt';
 import { BuildingAddressType } from './types';
 import Button from '@codegouvfr/react-dsfr/Button';
-import editPolygonIcon from '@/public/images/map/edition/edit_polygon.svg';
-import editPolygonDisabledIcon from '@/public/images/map/edition/edit_polygon_disabled.svg';
-import newPolygonIcon from '@/public/images/map/edition/new_polygon.svg';
 
 function PanelBody({ children }: { children: React.ReactNode }) {
   return <div className={styles.body}>{children}</div>;
@@ -76,20 +74,6 @@ function EditSelectedBuildingPanelContent({
     }
   }, [error, success]);
 
-  const handleBuildingShapeModification = () => {
-    dispatch(Actions.map.setDrawMode('direct_select'));
-  };
-
-  const handleBuildingShapeCreation = () => {
-    if (drawMode === 'draw_polygon') {
-      // that's a way to cancel the ongoing drawing
-      dispatch(Actions.map.setDrawMode(null));
-      dispatch(Actions.map.setBuildingNewShape(null));
-    } else {
-      dispatch(Actions.map.setDrawMode('draw_polygon'));
-    }
-  };
-
   const handleSubmit = async () => {
     setError(null);
     setSuccess(false);
@@ -135,62 +119,10 @@ function EditSelectedBuildingPanelContent({
           addresses={localAddresses}
           onChange={handleEditAddress}
         />
-        <div className={styles.panelSection}>
-          <span className={`fr-text--xs ${styles.sectionTitle}`}>
-            Géométrie
-          </span>
-
-          <div className="">
-            <span style={{ display: 'inline-block', width: 250 }}>
-              <Button
-                size="small"
-                onClick={handleBuildingShapeModification}
-                priority={`tertiary${drawMode === 'direct_select' ? '' : ' no outline'}`}
-                disabled={selectedBuilding.shape.type === 'Point'}
-              >
-                <img
-                  src={
-                    selectedBuilding.shape.type === 'Point'
-                      ? editPolygonDisabledIcon.src
-                      : editPolygonIcon.src
-                  }
-                  width="30"
-                  title="Modifier la géométrie existante"
-                ></img>
-                <span className="fr-pl-2v" style={{ textAlign: 'left' }}>
-                  Modifier la géometrie existante
-                </span>
-              </Button>
-            </span>
-
-            {drawMode === 'direct_select' && (
-              <div className="fr-text--xs fr-pt-1v fr-mb-1v">
-                Modifiez la géométrie du bâtiment directement sur la carte en
-                déplacant les sommets du polygone.
-              </div>
-            )}
-            <span style={{ display: 'inline-block', width: 250 }}>
-              <Button
-                size="small"
-                onClick={handleBuildingShapeCreation}
-                priority={`tertiary${drawMode === 'draw_polygon' ? '' : ' no outline'}`}
-              >
-                <img
-                  src={newPolygonIcon.src}
-                  width="30"
-                  title="Dessiner une nouvelle géométrie"
-                ></img>
-                <span className="fr-pl-2v" style={{ textAlign: 'left' }}>
-                  Dessiner une géométrie en partant de 0
-                </span>
-              </Button>
-            </span>
-            <div className="fr-text--xs fr-pt-1v">
-              {drawMode === 'draw_polygon' &&
-                'Dessinez une géométrie du bâtiment en cliquant sur la carte. Pour finaliser la géométrie, fermez le polygone en recliquant sur le premier point.'}
-            </div>
-          </div>
-        </div>
+        <BuildingShape
+          drawMode={drawMode}
+          selectedBuilding={selectedBuilding}
+        ></BuildingShape>
       </PanelBody>
       <div className={styles.footer}>
         <Button onClick={handleSubmit} disabled={!anyChanges}>
