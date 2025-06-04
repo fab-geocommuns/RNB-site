5c637ff7fbf0496e1cc2458e29fa969449b57519
Francis Chabouis <fchabouis@gmail.com>
2025-06-04 17:25:45 +0200

    plug it to the store

--- stores/store.tsx
+++ stores/store.tsx
@@ -3,12 +3,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { mapActions, mapReducer } from '@/stores/map/map-slice';
 import { appActions, appReducer } from '@/stores/app/app-slice';
-import { listenerMiddleware } from '@/stores/map/map-slice';
+import { editionActions, editionReducer } from '@/stores/edition/edition-slice';
+import { listenerMiddleware } from '@/stores/edition/edition-slice';
 
 export const store = configureStore({
   reducer: {
     map: mapReducer,
     app: appReducer,
+    edition: editionReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().prepend(listenerMiddleware.middleware),
@@ -20,4 +22,5 @@ export type AppDispatch = typeof store.dispatch;
 export const Actions = {
   map: mapActions,
   app: appActions,
+  edition: editionActions,
 };
