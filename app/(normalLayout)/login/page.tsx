4db74705c9b76bdc6e20e5cd7ee330d545a98e02
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-07-07 16:09:00 +0200

    similar block for create accounta nd login

--- app/(normalLayout)/login/page.tsx
+++ app/(normalLayout)/login/page.tsx
@@ -23,7 +23,7 @@ export default async function LoginPage() {
           <div className="fr-grid-row fr-grid-row--gutters">
             <div className="fr-col-12 fr-col-md-6 fr-col-lg-6">
               <div
-                className={`fr-container ${styles.loginShell} fr-px-md-12v fr-py-10v fr-py-md-14v `}
+                className={`fr-container ${styles.shell} fr-px-md-12v fr-py-10v fr-py-md-14v `}
               >
                 <h2>Se connecter au RNB</h2>
                 <div>
@@ -31,41 +31,17 @@ export default async function LoginPage() {
                 </div>
               </div>
             </div>
-            <div className="fr-col-12 fr-col-md-5 fr-col-offset-md-1">
-              {enableCreateAccount && (
-                <div className="fr-p-3w">
-                  <span className="fr-text--bold">
-                    Je n&apos;ai pas encore de compte
-                  </span>
-                  <h3>Créer un compte</h3>
-                  <CreateAccountForm />
-                </div>
-              )}
-              {!enableCreateAccount && (
-                <>
-                  <h6>Participer au RNB</h6>
-                  <p>
-                    Le Référentiel National des Bâtiments s&apos;ouvre aux
-                    contributions d&apos;une variété d&apos;acteurs publics,
-                    privés et citoyens.
-                  </p>
-                  <p>
-                    Si vous souhaitez participer à l&apos;enrichissement du RNB,
-                    vous pouvez demander un accès en remplissant le formulaire
-                    suivant.
-                  </p>
-                  <p>
-                    <a
-                      href="https://tally.so/r/npJJjP"
-                      target="_blank"
-                      className="fr-btn fr-btn--secondary"
-                    >
-                      Demander un accès
-                    </a>
-                  </p>
-                  <p></p>
-                </>
-              )}
+            <div className="fr-col-12 fr-col-md-6 fr-col-lg-6">
+              <div
+                className={`fr-container ${styles.shell} fr-px-md-12v fr-py-10v fr-py-md-14v `}
+              >
+                {enableCreateAccount && (
+                  <>
+                    <h3>Créer un compte RNB</h3>
+                    <CreateAccountForm />
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
