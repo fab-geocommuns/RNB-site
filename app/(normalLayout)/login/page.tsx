f13d0bedef46c6b659cacf972c54770c20086fab
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-07-07 16:31:09 +0200

    add a login shell

--- app/(normalLayout)/login/page.tsx
+++ app/(normalLayout)/login/page.tsx
@@ -2,10 +2,14 @@
 import { getServerSession } from 'next-auth/next';
 import { redirect } from 'next/navigation';
 
-// Styles
-import styles from '@/styles/login.module.scss';
+// Components
 import LoginForm from '@/components/authentication/LoginForm';
 import CreateAccountForm from '@/components/authentication/CreateAccountForm';
+
+// Styles
+import styles from '@/styles/login.module.scss';
+
+import summerStyles from '@/styles/summerGames.module.scss';
 
 export default async function LoginPage() {
   // We don't want to allow users that are already logged in to access this page
@@ -21,6 +25,20 @@ export default async function LoginPage() {
       <main className="fr-pt-md-14v" role="main">
         <div className="fr-container fr-container--fluid fr-mb-md-14v">
           <div className="fr-grid-row fr-grid-row--gutters">
+            <div className="fr-col-12 fr-col-md-8 fr-col-offset-md-2 fr-mb-8v ">
+              <div className={summerStyles.loginShell}>
+                <h2 className={summerStyles.loginShellTitle}>
+                  L&apos;expérience collaborative de l&apos;été
+                </h2>
+                <p>
+                  Créez un compte pour participer à l'expérimentation
+                  collaborative organisée par le RNB. Une fois connecté, vous
+                  pourrez directement éditer le référentiel et faire monter son
+                  niveau de qualité.
+                </p>
+              </div>
+            </div>
+
             <div className="fr-col-12 fr-col-md-6 fr-col-lg-6">
               <div
                 className={`fr-container ${styles.shell} fr-px-md-12v fr-py-10v fr-py-md-14v `}
