0fa91728e64b9661be825c685db561d150ffb942
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-07-10 11:34:43 +0200

    retouches texte et display user score

--- components/summerGames/homeBlock.tsx
+++ components/summerGames/homeBlock.tsx
@@ -104,7 +104,7 @@ export default function SummerGame({
                   </div>
 
                   <div className={styles.score}>
-                    <div className={styles.scoreAction}>Modifier le statut</div>
+                    <div className={styles.scoreAction}>Corriger le statut</div>
                     <div className={styles.scoreReward}>
                       1 <span className={styles.scoreRewardPoint}>point</span>
                     </div>
