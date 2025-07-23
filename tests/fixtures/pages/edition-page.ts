f55aa5b2999e60b519a5403676eb45bf234def49
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-07-23 14:33:15 +0200

    Refactor login in base page

--- tests/fixtures/pages/edition-page.ts
+++ tests/fixtures/pages/edition-page.ts
@@ -5,23 +5,16 @@ export class EditionPage extends RNBPage {
   readonly panel: Locator;
   readonly toaster: Locator;
   readonly map: Locator;
-  readonly myAccountButton: Locator;
-  readonly loginForm: Locator;
 
   constructor(page: Page) {
     super(page, '/edition');
     this.map = page.getByTestId('map');
     this.panel = page.getByTestId('edition-panel');
     this.toaster = page.getByTestId('toaster');
-    this.myAccountButton = page.getByTestId('my-account-button');
-    this.loginForm = page.getByTestId('login-form');
   }
 
   async goToBuilding(buildingId: string) {
-    const isLoggedIn = await this.myAccountButton.isVisible();
-    if (!isLoggedIn) {
-      await this.login();
-    }
+    await this.loginIfNotLoggedIn();
     await this.page.goto(`/edition?q=${buildingId}`);
   }
 
@@ -31,17 +24,5 @@ export class EditionPage extends RNBPage {
     );
     await deactivationButton.scrollIntoViewIfNeeded();
     await deactivationButton.click();
-  }
-
-  async login() {
-    await this.page.goto('/login');
-    await this.loginForm
-      .getByLabel('Email')
-      .fill(process.env.TEST_ACCOUNT_EMAIL!);
-    await this.loginForm
-      .getByLabel('Mot de passe')
-      .fill(process.env.TEST_ACCOUNT_PASSWORD!);
-    await this.loginForm.getByRole('button', { name: /se connecter/i }).click();
-    await this.page.waitForLoadState('networkidle');
   }
 }
