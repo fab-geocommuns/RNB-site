420a1f726c8059309e6385b1ff6c36bbaa43243f
Andreani Jean <ja.andreani@gmail.com>
2025-07-24 21:31:21 +0200

    fix sentry bug

--- components/map/VisuMapReact.tsx
+++ components/map/VisuMapReact.tsx
@@ -51,38 +51,6 @@ const VisuMapReact = ({ point, shape }: VisuMapReactProps) => {
       features,
     };
   }
-  // useEffect(() => {
-  //   const features: Feature[] = [];
-
-  //   if (point) {
-  //     features.push({
-  //       type: 'Feature',
-  //       properties: { type: 'point' },
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: point.coordinates,
-  //       },
-  //     } as Feature<Point>);
-  //   }
-
-  //   if (shape) {
-  //     features.push({
-  //       type: 'Feature',
-  //       properties: { type: 'polygon' },
-  //       geometry: {
-  //         type: 'MultiPolygon',
-  //         coordinates: shape.coordinates,
-  //       },
-  //     } as Feature<MultiPolygon>);
-  //   }
-  //   console.log('features', features)
-  //   setBuilding({
-  //     type: 'FeatureCollection',
-  //     features: features
-  //   })
-  //   console.log('building', building)
-  // }, [point, shape]);
-  console.log(formatBuilding());
   // Layer styles
   const polygonLayer: LayerProps = {
     id: 'building-polygon',
@@ -141,57 +109,7 @@ const VisuMapReact = ({ point, shape }: VisuMapReactProps) => {
             `${process.env.NEXT_PUBLIC_API_BASE}/tiles/shapes/{x}/{y}/{z}.pbf?only_active_and_real=false`,
           ]}
         ></Source>
-        <Source
-          id="building-source"
-          type="geojson"
-          data={{
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                properties: {
-                  type: 'point',
-                },
-                geometry: {
-                  type: 'Point',
-                  coordinates: [2.400800129, 48.861114479],
-                },
-              },
-              {
-                type: 'Feature',
-                properties: {
-                  type: 'polygon',
-                },
-                geometry: {
-                  type: 'MultiPolygon',
-                  coordinates: [
-                    [
-                      [
-                        [2.400553942, 48.8611944],
-                        [2.400534912, 48.861189808],
-                        [2.400597674, 48.861066013],
-                        [2.400620782, 48.861071524],
-                        [2.400670552, 48.861010618],
-                        [2.400757482, 48.861070939],
-                        [2.400761715, 48.861024565],
-                        [2.400993975, 48.861095877],
-                        [2.40099952, 48.86108781],
-                        [2.401036186, 48.861099685],
-                        [2.401018126, 48.861129273],
-                        [2.400964143, 48.86120095],
-                        [2.400780794, 48.861143374],
-                        [2.400753111, 48.861180108],
-                        [2.400762621, 48.861182854],
-                        [2.400716919, 48.861245579],
-                        [2.400553942, 48.8611944],
-                      ],
-                    ],
-                  ],
-                },
-              },
-            ],
-          }}
-        >
+        <Source id="building-source" type="geojson" data={formatBuilding()}>
           <Layer {...polygonLayer} />
           <Layer {...polygonOutlineLayer} />
           <Layer {...pointLayer} />
