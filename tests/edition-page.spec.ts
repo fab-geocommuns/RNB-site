e8aa30bdc0407e89e65391d424bc8c3cfda178c2
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-07-24 12:10:29 +0200

    Fix banner error not showing on wrong split

--- tests/edition-page.spec.ts
+++ tests/edition-page.spec.ts
@@ -105,5 +105,78 @@ test.describe('Edition', () => {
         editionPage.toaster.getByText(/scindé avec succès/i),
       ).toBeVisible();
     });
+
+    test('doit pouvoir afficher une erreur de validation sur un des enfants', async ({
+      editionPage,
+      httpMocker,
+      mapController,
+    }) => {
+      const targetBuilding = '6NFTV4Z6DP92';
+      await editionPage.goToBuilding(targetBuilding);
+      await expect(editionPage.panel).toBeVisible();
+
+      const map = mapController('mainMap');
+
+      await map.fitMapToBounds([2.421027, 48.844011, 2.428424, 48.84669]);
+
+      await editionPage.startSplit();
+      await editionPage.clickNext();
+
+      await editionPage.drawShape(map, [
+        [2.423719541, 48.845236854],
+        [2.42375453, 48.845399805],
+        [2.424280374, 48.845348939],
+        [2.424247375, 48.845183219],
+        [2.423719541, 48.845236854],
+      ]);
+
+      await editionPage.clickNext();
+      await editionPage.drawShape(map, [
+        [2.424247375, 48.845183219],
+        [2.424280374, 48.845348939],
+        [2.425280374, 48.845248939],
+        [2.425247375, 48.845083219],
+        [2.424247375, 48.845183219],
+      ]);
+
+      await httpMocker.mockAPIRequest(
+        'POST',
+        `/buildings/${targetBuilding}/split/?from=site`,
+        {
+          created_buildings: [
+            {
+              status: 'constructed',
+              addresses_cle_interop: [],
+              shape:
+                'POLYGON ((2.423721634213109 48.84523835886833, 2.4237562502758863 48.84539782325717, 2.4242824144108397 48.84534770593274, 2.4242477983494553 48.84518368524684, 2.423721634213109 48.84523835886833))',
+            },
+            {
+              status: 'constructed',
+              addresses_cle_interop: [],
+              shape:
+                'POLYGON ((2.4242477983494553 48.84518368524684, 2.4242824144108397 48.84534770593274, 2.4252793569856124 48.84524747113295, 2.425244740924228 48.845083450118295, 2.4242477983494553 48.84518368524684))',
+            },
+          ],
+        },
+        {
+          status: 400,
+          body: {
+            created_buildings: {
+              '0': {
+                shape: [
+                  "La forme fournie n'a pas pu être analysée ou n'est pas valide",
+                ],
+              },
+            },
+          },
+        },
+        true,
+      );
+
+      await editionPage.confirmSplit();
+      await expect(
+        editionPage.toaster.getByText(/1er.*n'est pas valide/i),
+      ).toBeVisible();
+    });
   });
 });
