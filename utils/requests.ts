aac7fa71a061111076331a9d874a6866fbf446ec
Francis Chabouis <fchabouis@gmail.com>
2025-08-11 16:46:13 +0200

    missing / creates a redirect

--- utils/requests.ts
+++ utils/requests.ts
@@ -2,7 +2,7 @@ import { bdgApiUrl } from '@/stores/map/map-slice';
 import { SelectedBuilding } from '@/stores/map/map-slice';
 
 export async function fetchBuilding(rnbId: string) {
-  const url = bdgApiUrl(rnbId + '?from=site&withPlots=1');
+  const url = bdgApiUrl(`${rnbId}/?from=site&withPlots=1`);
   const rnbResponse = await fetch(url);
   if (rnbResponse.ok) {
     const rnbData = (await rnbResponse.json()) as SelectedBuilding;
