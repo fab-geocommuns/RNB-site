1bddb80db93aa0f6dce9ec287c19beaacdf1d36c
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-08-12 16:17:33 +0200

    WIP idea for different search placemenet

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,29 @@
 import { fr } from '@codegouvfr/react-dsfr';
 import Button from '@codegouvfr/react-dsfr/Button';
 import mapImageSource from './map.png';
+import AddressSearchHome from '../address/AddressSearchHome';
+
+function Illustration() {
+  return (
+    <div
+      className={fr.cx('fr-p-5v')}
+      style={{
+        backgroundImage: `url(${mapImageSource.src})`,
+        //aspectRatio: "16 / 9",
+        height: '100%',
+        backgroundPosition: 'center',
+        backgroundSize: 'auto',
+        borderRadius: '16px',
+        boxShadow: '0px 0px 20px -3px rgba(0,0,0,0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'end',
+      }}
+    >
+      <AddressSearchHome />
+    </div>
+  );
+}
 
 export default function Hero() {
   console.log(mapImageSource);
@@ -14,36 +37,26 @@ export default function Hero() {
           <div className={fr.cx('fr-col-6', 'fr-pt-18v', 'fr-pb-18v')}>
             <h1>Bienvenue au Référentiel National des Bâtiments</h1>
             <p>
-              Le Référentiel national des bâtiments (RNB) est un service public
-              numérique qui répertorie l'ensemble des bâtiments du territoire et
-              leur associe un identifiant unique.
+              Le Référentiel national des bâtiments (RNB) est un{' '}
+              <b>service public numérique</b> qui{' '}
+              <b>répertorie l'ensemble des bâtiments du territoire</b> et leur
+              associe un <b>identifiant unique</b>.
             </p>
             <p>
-              Cet ID-RNB est facilement transmissible par une personne ou un
-              logiciel et permet de croiser facilement les jeux de données
-              bâtimentaires, pour les administrations publiques, les
-              collectivités et les acteurs privés.
-              <br /> <u>En savoir plus</u>
+              Cet <b>ID-RNB</b> est facilement transmissible par une personne ou
+              un logiciel et permet de{' '}
+              <b>croiser facilement les jeux de données bâtimentaires</b>, pour
+              les administrations publiques, les collectivités et les acteurs
+              privés.
             </p>
             <p>
-              <Button iconId="fr-icon-road-map-line">Consulter la carte</Button>{' '}
-              <Button priority="secondary" iconId="fr-icon-search-line">
-                Rechercher un bâtiment
-              </Button>
+              {' '}
+              <Button>Voir la carte des bâtiments</Button>{' '}
+              <Button priority="tertiary no outline">En savoir plus</Button>
             </p>
           </div>
           <div className={fr.cx('fr-col-6', 'fr-p-18v')}>
-            <div
-              style={{
-                backgroundImage: `url(${mapImageSource.src})`,
-                //aspectRatio: "16 / 9",
-                height: '100%',
-                backgroundPosition: 'center',
-                backgroundSize: 'auto',
-                borderRadius: '16px',
-                boxShadow: '0px 0px 20px -3px rgba(0,0,0,0.1)',
-              }}
-            ></div>
+            <Illustration />
           </div>
         </div>
       </div>
