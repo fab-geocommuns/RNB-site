4086112fbb51133b1c04483fea35ec01c8d19085
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-08-26 13:37:55 +0200

    Test for blog-based use-cases

--- components/home/UseCases.tsx
+++ components/home/UseCases.tsx
@@ -1,6 +1,12 @@
-import Tile from '@codegouvfr/react-dsfr/Tile';
+import { PostsOrPages } from '@tryghost/content-api';
+import Card from '@codegouvfr/react-dsfr/Card';
 
-export default function UseCases() {
+type Props = {
+  useCases: PostsOrPages;
+};
+
+export default function UseCases({ useCases }: Props) {
+  const featuredUseCase = useCases.slice(0, 3);
   return (
     <div className="section">
       <div className="fr-grid-row fr-grid-row--gutters">
@@ -13,28 +19,17 @@ export default function UseCases() {
           </div>
 
           <div className="fr-grid-row fr-grid-row--gutters">
-            <div className="fr-col-12 fr-col-md-4 ">
-              <Tile
-                desc="La possibilité d’indiquer dans chaque DPE l’identifiant unique du bâtiment dans lequel le local se situe permettra ainsi d’améliorer la fiabilité des bases de données de l’observatoire des DPE ..."
-                linkProps={{ href: '/cas/dpe' }}
-                title="Fiabiliser les Diagnostics de Performance Énergétique (DPE)"
-              />
-            </div>
-            <div className="fr-col-12 fr-col-md-4 ">
-              <Tile
-                desc="L’existence du RNB doit permettre à toutes les communes d’assurer la cohérence du système d’information et la transversalité des données de la collectivité entre les différents services ..."
-                linkProps={{ href: '/cas/communes' }}
-                title="Faciliter les échanges d’informations bâtimentaires au sein des communes"
-              />
-            </div>
-
-            <div className="fr-col-12 fr-col-md-4 ">
-              <Tile
-                desc="La saisie d’un ID bâtiment lors du processus de référencement des ERP doit permettre de préciser leur géolocalisation, de faciliter et fiabiliser le processus de recensement et, d’aider le suivi des ERP."
-                linkProps={{ href: '/cas/erp' }}
-                title="Améliorer l’identification des Etablissements Recevant du Public (ERP)"
-              />
-            </div>
+            {featuredUseCase.map((useCase) => (
+              <div className="fr-col-12 fr-col-md-4 " key={useCase.id}>
+                <Card
+                  imageUrl={useCase.feature_image || ''}
+                  imageAlt={useCase.feature_image_alt || ''}
+                  desc={useCase.excerpt}
+                  linkProps={{ href: '/cas/' + useCase.slug }}
+                  title={useCase.title}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
