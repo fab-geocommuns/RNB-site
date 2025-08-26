96f984664bdf80d423f73ecee1b1b1b1b0b7fbe6
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-08-26 16:59:17 +0200

    Test logos on Home

--- components/home/UseCases.tsx
+++ components/home/UseCases.tsx
@@ -1,6 +1,9 @@
 import { PostsOrPages } from '@tryghost/content-api';
-import Card from '@codegouvfr/react-dsfr/Card';
 import Tile from '@codegouvfr/react-dsfr/Tile';
+import nantes from './logos/nantes.png';
+import echirolles from './logos/echirolles.png';
+import smh from './logos/smh.jpg';
+import auditDpe from './logos/audit-dpe.jpeg';
 
 // Styles
 import styles from '@/styles/home.module.scss';
@@ -11,6 +14,20 @@ type Props = {
 
 export default function UseCases({ useCases }: Props) {
   const featuredUseCase = useCases.slice(0, 3);
+  const logos = [
+    {
+      src: nantes.src,
+    },
+    {
+      src: echirolles.src,
+    },
+    {
+      src: smh.src,
+    },
+    {
+      src: auditDpe.src,
+    },
+  ];
   return (
     <div className="section">
       <div className="fr-grid-row fr-grid-row--gutters">
@@ -25,18 +42,31 @@ export default function UseCases({ useCases }: Props) {
           <div className="fr-grid-row fr-grid-row--gutters">
             {featuredUseCase.map((useCase) => (
               <div className="fr-col-12 fr-col-md-4 " key={useCase.id}>
-                <Card
-                  background
-                  border
-                  enlargeLink
+                <Tile
                   className={styles['card-use-case']}
                   imageUrl={useCase.feature_image || ''}
                   imageAlt={useCase.feature_image_alt || ''}
                   linkProps={{ href: '/cas/' + useCase.slug }}
                   title={useCase.title}
+                  desc={useCase.excerpt}
                 />
               </div>
             ))}
+          </div>
+
+          {/* Logo Carousel */}
+          <div className={styles['logo-carousel']} style={{ maxWidth: 800 }}>
+            <div className={styles['logo-carousel__track']}>
+              {[...logos, ...logos, ...logos].map((logo, index) => (
+                <div key={index} className={styles['logo-carousel__item']}>
+                  <img
+                    src={logo.src}
+                    alt={'logo'}
+                    className={styles['logo-carousel__logo']}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
