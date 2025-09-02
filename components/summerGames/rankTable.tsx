256868ac7f1a9d91633f2e7a958e6f1945f54608
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-09-02 11:35:05 +0200

    better display for big scores

--- components/summerGames/rankTable.tsx
+++ components/summerGames/rankTable.tsx
@@ -1,9 +1,22 @@
 // Styles
 import styles from '@/styles/summerGames.module.scss';
 
+import Tooltip from '@codegouvfr/react-dsfr/Tooltip';
+
 type RankTableProps = {
   title: string;
   ranks: any[];
+};
+
+const bigScoreLimit = 10000;
+
+const formatCount = (count: number) => {
+  if (count >= bigScoreLimit) {
+    const rounded = Math.round(count / 100) / 10;
+    return rounded.toString().replace('.', ',') + 'K';
+  }
+  //return count;
+  return count.toLocaleString('fr-FR');
 };
 
 export default function RankTable({ title, ranks }: RankTableProps) {
@@ -16,7 +29,14 @@ export default function RankTable({ title, ranks }: RankTableProps) {
           <div key={index} className={styles.rankRow}>
             <div className={styles.rankMedalShell}>{index + 1} •</div>
             <div className={styles.rankNameShell}>{rank.name}</div>
-            <div className={styles.rankCountShell}>{rank.count}</div>
+            <div className={styles.rankCountShell}>
+              {rank.count >= bigScoreLimit && (
+                <Tooltip kind="hover" title={rank.count}>
+                  {formatCount(rank.count)}
+                </Tooltip>
+              )}
+              {rank.count < bigScoreLimit && <>{formatCount(rank.count)}</>}
+            </div>
           </div>
         ))}
       </div>
