82770c3db63d79e69131d65336115726fcecfd8a
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-09-02 11:28:51 +0200

    fix big score display

--- components/summerGames/rankTable.tsx
+++ components/summerGames/rankTable.tsx
@@ -6,6 +6,15 @@ type RankTableProps = {
   ranks: any[];
 };
 
+const formatCount = (count: number) => {
+  if (count >= 10000) {
+    const rounded = Math.round(count / 100) / 10;
+    return rounded.toString().replace('.', ',') + 'K';
+  }
+  return count;
+  //return count.toLocaleString('fr-FR');
+};
+
 export default function RankTable({ title, ranks }: RankTableProps) {
   return (
     <div className={styles.rankShell}>
@@ -16,7 +25,9 @@ export default function RankTable({ title, ranks }: RankTableProps) {
           <div key={index} className={styles.rankRow}>
             <div className={styles.rankMedalShell}>{index + 1} •</div>
             <div className={styles.rankNameShell}>{rank.name}</div>
-            <div className={styles.rankCountShell}>{rank.count}</div>
+            <div className={styles.rankCountShell}>
+              {formatCount(rank.count)}
+            </div>
           </div>
         ))}
       </div>
