5aef7f53e3574e4ff21b96b53107bcb946df1ed9
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-09-17 17:09:40 +0200

    MàJ outils

--- components/home/ToolsAndServices.tsx
+++ components/home/ToolsAndServices.tsx
@@ -62,7 +62,7 @@ function TabBox({ tabs }: TabBoxProps) {
             ))}
           </ul>
         }
-        size="large"
+        size="medium"
         imageUrl={tabs[activeTab].content.image}
         imageAlt={tabs[activeTab].title}
       />
@@ -77,19 +77,24 @@ export default function ToolsAndServices() {
       icon: '📖',
       content: {
         image: rapprochementIllu.src,
-        description: <>Consultez les données libres et ouvertes du RNB.</>,
+        description: (
+          <>
+            <b>Consultez les données libres et ouvertes du RNB</b>, directement
+            sur la carte ou via nos APIs et jeux de données.
+          </>
+        ),
         links: [
           {
             title: 'Carte des bâtiments',
-            link: '#',
+            link: '/carte',
           },
           {
             title: 'Documentation de nos APIs',
-            link: '#',
+            link: '/doc',
           },
           {
             title: 'Définition et standard',
-            link: '#',
+            link: '/definition',
           },
         ],
       },
@@ -101,53 +106,51 @@ export default function ToolsAndServices() {
         image: apiIllu.src,
         description: (
           <>
-            Vous produisez ou exploitez des données bâtimentaires ? Intégrez
-            Vous produisez ou exploitez des données bâtimentaires ? Intégrez le
-            RNB dans votre flux, directement sur votre application ou via un
-            processus de traitement.
+            Vous produisez ou exploitez des données bâtimentaires ?<br />
+            <b>Intégrez le RNB dans votre flux</b>, directement sur votre
+            application ou via un processus de traitement.
           </>
         ),
         links: [
           {
             title: 'API et jeux de données',
-            link: '#',
+            link: '/doc',
           },
           {
             title: 'Selecteur de bâtiments',
-            link: '#',
+            link: 'https://rnb-fr.gitbook.io/documentation/exemples/selecteur-de-batiments',
           },
           {
             title: 'Définition et standard',
-            link: '#',
+            link: '/definition',
           },
         ],
       },
     },
     {
-      title: 'Alimenter',
+      title: 'Contribuer',
       icon: '🔄',
       content: {
         image: adsIllu.src,
         description: (
           <>
-            Partagez la connaissance de votre territoire et bénéficiez en
-            Partagez la connaissance de votre territoire et bénéficiez en retour
-            des améliorations apportées par la communauté pour une donnée
+            <b>Partagez la connaissance de votre territoire</b> et bénéficiez en
+            retour des améliorations apportées par la communauté pour une donnée
             bâtimentaire la plus proche du terrain !
           </>
         ),
         links: [
           {
             title: "API d'édition",
-            link: '#',
+            link: 'https://rnb-fr.gitbook.io/documentation/api-et-outils/api-batiments/editer-le-rnb',
           },
           {
             title: 'Carte interactive',
-            link: '#',
+            link: '/edition',
           },
           {
-            title: 'Définition et standard',
-            link: '#',
+            title: "Guide d'édition",
+            link: 'https://rnb-fr.gitbook.io/documentation/api-et-outils/api-batiments/editer-le-rnb/guide-dedition-du-rnb',
           },
         ],
       },
