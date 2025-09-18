f433f35dc636c1e4d2f6a285cf5067c05e975627
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-09-18 15:40:26 +0200

    Extract hero CSS

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,6 +4,7 @@ import mapImageSource from './map.png';
 import mapImageSource2 from './map1.png';
 import mapImageSource3 from './map2.png';
 import AddressSearchHome from '../address/AddressSearchHome';
+import styles from '../../styles/home/hero.module.scss';
 
 function Illustration({ children }: { children: React.ReactNode }) {
   const images = [
@@ -23,69 +24,18 @@ function Illustration({ children }: { children: React.ReactNode }) {
   ];
   return (
     <>
-      <style>
-        {`.image-carousel {
-        display: flex;
-        flex-direction: column;
-        justify-content: end;
-        position: relative;
-        overflow: hidden;
-      }
-      .image-carousel .image {
-        position: absolute;
-        top: 0;
-  left: 0;  
-  right: 0;
-  bottom: 0;
-  background-size: cover;
-  background-position: center;
-  opacity: 0;
-        height: 100%;
-        box-shadow: 0px 0px 20px -3px rgba(0,0,0,0.1);
-        border-radius: 16px;
-      animation: drift 15s infinite, fade 15s infinite;
-      }
-
-      .image-carousel-content {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: 1;
-      }
-
-
-.image:nth-child(1) { animation-delay: 0s; }
-.image:nth-child(2) { animation-delay: 5s; }
-.image:nth-child(3) { animation-delay: 10s; }
-
-@keyframes drift {
-  0% {
-    background-position: center left; /* Start at the left */
-  }
-  100% {
-    background-position: center right; /* Stay at the right */
-  }
-}
-
-@keyframes fade {
-  0%, 100% { opacity: 0; }
-  10%, 30% { opacity: 1; }
-}
-      `}
-      </style>
       <div
-        className={`image-carousel`}
+        className={styles.imageCarousel}
         style={{ height: '100%', width: '100%' }}
       >
         {images.map((image) => (
           <div
-            className={`image`}
+            className={styles.imageCarouselImage}
             style={{ backgroundImage: `url(${image.src})` }}
             key={image.alt}
           ></div>
         ))}
-        <div className={`image-carousel-content ${fr.cx('fr-p-5v')}`}>
+        <div className={styles.imageCarouselContent + ' ' + fr.cx('fr-p-5v')}>
           {children}
         </div>
       </div>
