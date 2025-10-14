d492874afe306b7ae181e2ed35a9fbb2224d9a8d
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-10-14 11:50:55 +0200

    Cleanup

--- middleware.ts
+++ middleware.ts
@@ -14,15 +14,26 @@ function cspMiddleware(request: NextRequest) {
     'https://openmaptiles.geo.data.gouv.fr/',
     'https://openmaptiles.github.io/',
   ];
+  const sentryOrigin = 'https://sentry.incubateur.net/';
+  const connectOrigins = [
+    rnbApiOrigin,
+    ...banApiOrigins,
+    ...tileOrigins,
+    sentryOrigin,
+  ];
+  const mediaOrigins = [
+    'https://rnb-open.s3.fr-par.scw.cloud/',
+    'https://referentiel-national-du-batiment.ghost.io/',
+  ];
   // We allow `unsafe-inline` for the style-src directive because of https://github.com/vercel/next.js/issues/57415
   // When it's fixed, we can remove it.
   const cspHeader = `
     default-src 'self';
-    connect-src 'self' ${rnbApiOrigin} ${banApiOrigins.join(' ')} ${tileOrigins.join(' ')};
+    connect-src 'self' ${connectOrigins.join(' ')};
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ''};
     style-src 'self' 'unsafe-inline';
-    media-src 'self' blob: data: https://rnb-open.s3.fr-par.scw.cloud/ https://referentiel-national-du-batiment.ghost.io/;
-    img-src 'self' blob: data: https://rnb-open.s3.fr-par.scw.cloud/ https://referentiel-national-du-batiment.ghost.io/;
+    media-src 'self' blob: data: ${mediaOrigins.join(' ')};
+    img-src 'self' blob: data: ${mediaOrigins.join(' ')};
     font-src 'self';
     object-src 'none';
     base-uri 'self';
@@ -54,14 +65,6 @@ function cspMiddleware(request: NextRequest) {
   );
 
   return response;
-}
-
-function noopMiddleware(request: NextRequest) {
-  return NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
 }
 
 export function middleware(request: NextRequest) {
