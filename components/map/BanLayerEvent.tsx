d9e5bab4816429557ba6fa205212c1e3e1b7a408
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-10-30 09:31:52 +0100

    ecape ban output

--- components/map/BanLayerEvent.tsx
+++ components/map/BanLayerEvent.tsx
@@ -1,20 +1,40 @@
 import maplibregl from 'maplibre-gl';
 
+const escapeHtml = (value: unknown) =>
+  String(value ?? '').replace(/[&<>"']/g, (char) => {
+    switch (char) {
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '>':
+        return '&gt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&#39;';
+      default:
+        return char;
+    }
+  });
+
 export const displayBANPopup = (map: maplibregl.Map, addressFeature: any) => {
   let certStr = 'Adresse BAN non certifiée';
   if (addressFeature.properties.certifie) {
     certStr = 'Adresse BAN certifiée';
   }
 
+  const props = addressFeature?.properties ?? {};
+
   new maplibregl.Popup()
     .setLngLat(addressFeature.geometry.coordinates)
     .setHTML(
       `
       <div class="banPop">
-      <h6 class="banPopTitle">${certStr}</h6>
-      <p class="banPopBody">${addressFeature.properties.numero || ''} ${addressFeature.properties.suffixe || ''}  ${addressFeature.properties.nomVoie || ''}<br />
-      ${addressFeature.properties.nomCommune || ''}</p>
-      <p class="banPopFooter">Clé BAN : ${addressFeature.properties.id}</p>
+      <h6 class="banPopTitle">${escapeHtml(certStr)}</h6>
+      <p class="banPopBody">${escapeHtml(props.numero)} ${escapeHtml(props.suffixe)}  ${escapeHtml(props.nomVoie)}<br />
+      ${escapeHtml(props.nomCommune)}</p>
+      <p class="banPopFooter">Clé d'interopérabilité BAN : ${escapeHtml(props.id)}</p>
       </div>
        `,
     )
