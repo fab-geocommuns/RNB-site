861a155f8cb462f801ead2533aa62bfc2b9e15ed
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-11-03 18:08:45 +0100

    always new home and nav

--- app/(normalLayout)/page.tsx
+++ app/(normalLayout)/page.tsx
@@ -1,10 +1,5 @@
 import NewHome from './NewHome';
-import OldHome from './OldHome';
 
 export default function Page() {
-  if (process.env.NEXT_PUBLIC_NEW_HOME_ENABLED) {
-    return <NewHome />;
-  }
-
-  return <OldHome />;
+  return <NewHome />;
 }
