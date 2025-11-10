a13d5f58d6bb2c475f2ba7b08a3498bdb098ad52
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-11-10 17:18:23 +0100

    form en cours

--- components/map/report/ReportForm.tsx
+++ components/map/report/ReportForm.tsx
@@ -1,9 +1,44 @@
 import styles from '@/styles/report/form.module.scss';
+import { SegmentedControl } from '@codegouvfr/react-dsfr/SegmentedControl';
 
 export default function ReportForm({ report }: { report?: any }) {
   if (report.properties.status != 'pending') {
     return null;
   }
 
-  return <div className={styles.shell}>FORM</div>;
+  return (
+    <div className={styles.shell}>
+      <form method="post" action="/api/report/respond">
+        <input type="hidden" name="reportId" value={report.id} />
+        <div>
+          <label htmlFor="message">Votre message</label>
+          <textarea
+            className="fr-input"
+            id="message"
+            name="message"
+            required
+          ></textarea>
+        </div>
+        <SegmentedControl
+          small={true}
+          name="action"
+          legend="Votre action"
+          segments={[
+            {
+              label: 'Commenter',
+            },
+            {
+              label: 'Traiter',
+            },
+            {
+              label: 'Rejeter',
+            },
+          ]}
+        />
+        <div>
+          <button type="submit">Envoyer</button>
+        </div>
+      </form>
+    </div>
+  );
 }
