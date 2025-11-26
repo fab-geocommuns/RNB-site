ab91e11fb8e1d251aa5764e50b373c667ea43f82
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-11-26 12:39:50 +0100

    Fix incorrect dependencies

--- stores/edition/edition-selector.tsx
+++ stores/edition/edition-selector.tsx
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 import { SplitChild } from './edition-slice';
 import { RootState } from '../store';
 import { geojsonToReducedPrecisionWKT } from '@/utils/geojsonToReducedPrecisionWKT';
