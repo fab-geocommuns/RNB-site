dafe7990a7dcfcf622d70b62aec5ecc50f4ab938
Paul Etienney <paul@donatello.dev>
2025-11-28 17:16:46 +0100

    old view (#377)
    Co-authored-by: Paul Etienney <paul.etienney@beta.gouv.fr>

--- components/map/useMapLayers.ts
+++ components/map/useMapLayers.ts
@@ -2,6 +2,7 @@
 import vectorOsm from '@/components/map/mapstyles/vector-osm.json';
 import vectorIgnStandard from '@/components/map/mapstyles/vector-ign-standard.json';
 import satellite from '@/components/map/mapstyles/satellite.json';
+import satellite_2016_2020 from '@/components/map/mapstyles/satellite_2016_2020.json';
 
 // Maplibre styles
 import maplibregl, { StyleSpecification } from 'maplibre-gl';
@@ -94,6 +95,10 @@ export const STYLES: Record<
   satellite: {
     name: 'Satellite',
     style: satellite as StyleSpecification,
+  },
+  satellite_2016_2020: {
+    name: 'Satellite 2011-2016',
+    style: satellite_2016_2020 as StyleSpecification,
   },
 };
 
