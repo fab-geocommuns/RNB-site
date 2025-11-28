ebfb2cef94e2f6c5f62a52bf7f208eacd4a96e8f
Paul Etienney <paul.etienney@beta.gouv.fr>
2025-11-28 15:29:48 +0100

    throw error if report response is not ok

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,5 +1,6 @@
 import { bdgApiUrl } from '@/stores/map/map-slice';
 import { SelectedBuilding } from '@/stores/map/map-slice';
+import { throwErrorMessageForHumans } from '@/components/contribution/toaster';
 
 export async function fetchBuilding(rnbId: string) {
   const url = bdgApiUrl(`${rnbId}/?from=site&withPlots=1`);
@@ -19,5 +20,8 @@ export async function fetchReport(reportId: number) {
   const rnbResponse = await fetch(url);
   if (rnbResponse.ok) {
     return await rnbResponse.json();
+  } else {
+    // throws an error
+    await throwErrorMessageForHumans(rnbResponse);
   }
 }
