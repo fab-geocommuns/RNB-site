bf6afa244396fd141838c0b8ba7c5d2e5b63260f
Paul Etienney <paul.etienney@beta.gouv.fr>
2026-01-06 16:15:32 +0100

    feve and home

--- components/games/feve/leaderBoard.tsx
+++ components/games/feve/leaderBoard.tsx
@@ -2,14 +2,86 @@
 
 // Styles
 import styles from '@/styles/feve.module.scss';
-//import RankTable from './rankTable';
+import { useEffect, useState } from 'react';
+import { FeveData, useFeveData } from '@/utils/feve';
+import Image from 'next/image';
 
 export const revalidate = 10;
 
 export default function FeveLeaderBoard() {
+  const [loading, setLoading] = useState(true);
+  const [feveData, setFeveData] = useState<FeveData[]>();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await useFeveData();
+        const sortedData = data.sort((a, b) =>
+          a.dpt_name.localeCompare(b.dpt_name),
+        );
+        setFeveData(sortedData);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
-    <>
-      <div>FEVE</div>
-    </>
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <Image
+          src={`/images/feves/curl-left.png`}
+          alt=""
+          width={100}
+          height={43}
+        />
+        <h1 className={styles.title}>Les fèves du RNB</h1>
+        <Image
+          src={`/images/feves/curl-right.png`}
+          alt=""
+          width={100}
+          height={43}
+        />
+      </div>
+
+      <div className={styles.intro}>
+        <p>
+          <b>
+            Nouveau : les signalements permettent de trouver où améliorer le
+            référentiel.
+          </b>
+        </p>
+        <p>
+          Pour fêter le lancement de cette fonctionnalité, nous avons caché{' '}
+          {feveData?.length || 0} fèves sur tout le territoire. Traitez des
+          signalements pour tenter de les retrouver !
+        </p>
+      </div>
+      {!loading && feveData && (
+        <ul className={styles.grid}>
+          {feveData.map((dpt) => (
+            <li
+              key={dpt.dpt_code}
+              className={`${styles.item} ${dpt.feve_found_at ? styles.found : styles.not_found}`}
+            >
+              <Image
+                src={`/images/feves/feve-${dpt.dpt_code}.png`}
+                alt={dpt.dpt_name}
+                width={130}
+                height={130}
+              />
+              <span className={styles.department_name}>{dpt.dpt_name}</span>
+              {/* {dpt.feve_found_at && (
+                <span>Trouvée par {dpt.feve_found_by}</span>
+              )} */}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
