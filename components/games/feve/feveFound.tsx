1bf8996913db68b694b32fbbafe4304ab7ea766f
Paul Etienney <paul.etienney@beta.gouv.fr>
2026-01-08 12:18:55 +0100

    confetti

--- components/games/feve/feveFound.tsx
+++ components/games/feve/feveFound.tsx
@@ -2,6 +2,7 @@ import styles from '@/styles/feveFound.module.scss';
 import { createModal } from '@codegouvfr/react-dsfr/Modal';
 import Image from 'next/image';
 import { useEffect } from 'react';
+import confetti from 'canvas-confetti';
 
 const feveFoundModal = createModal({
   id: 'feve-found-modal',
@@ -9,10 +10,50 @@ const feveFoundModal = createModal({
 });
 
 export default function FeveFound({ showModal }: { showModal?: boolean }) {
+  const confettiCount = 200;
+  const confettiDefaults = {
+    colors: ['#B86949', '#E3A75B', '#472F4A'],
+    origin: { x: 0.5, y: 0.7 },
+    zIndex: 1751,
+  };
+  const fire = function (particleRatio: number, opts: any) {
+    confetti({
+      ...confettiDefaults,
+      ...opts,
+      particleCount: Math.floor(confettiCount * particleRatio),
+    });
+  };
+
   useEffect(() => {
     if (showModal !== false) {
       setTimeout(() => {
         feveFoundModal.open();
+
+        // Trigger confetti
+        setTimeout(() => {
+          fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
+          });
+          fire(0.2, {
+            spread: 60,
+          });
+          fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8,
+          });
+          fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2,
+          });
+          fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+          });
+        }, 50);
       }, 300);
     }
   }, [showModal]);
